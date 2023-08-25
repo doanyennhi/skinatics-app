a1c005b8117a0dbc738b62373b3037b81e8187cd
marielle-louisse <112800223+marielle-louisse@users.noreply.github.com>
2023-08-25 15:06:05 +1000

    added views for search, camera, routine

--- Skinatics/ContentView.swift
+++ Skinatics/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        SkinQuizView()
+        StartUpView()
+        // SkinQuizView()
+        TabView {
+            HomeView()
+        }
     }
         
 }
