527b1b02618aa2d4921b08e1ef612c317ac5b145
marielle-louisse <112800223+marielle-louisse@users.noreply.github.com>
2023-08-25 15:20:14 +1000

    started profile view

---
+++ Skinatics/View/Profile/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  Skinatics
+//
+//  Created by Marielle Louisse Lopez on 25/8/2023.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
--- Skinatics/View/Profile/ProfileView.swift
+++ Skinatics/View/Profile/ProfileView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+var profileOptions: [String] = ["Notifications", "Settings", "FAQs", "Get help", "Feedback", "Privacy and terms"]
+
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Jane Doe")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color("Dark Green"))
+                    
+                    NavigationLink(destination: EditProfileView()) {
+                        Text("View and edit profile")
+                            .foregroundColor(Color("AccentColor"))
+                    }
+                }
+                Spacer()
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80.0, height: 80.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color("Light Grey"), lineWidth: 1.0))
+            }
+            
+        }
+        .padding(40)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("Floral white"))
     }
 }
 
