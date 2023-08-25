03fec90f518988c2e92318ad7cf131addb446b23
marielle-louisse <112800223+marielle-louisse@users.noreply.github.com>
2023-08-25 14:38:09 +1000

    added secondary button style

---
+++ Skinatics/View/StartUp/StartUpView.swift
@@ -0,0 +1,20 @@
+//
+//  StartUpView.swift
+//  Skinatics
+//
+//  Created by Marielle Louisse Lopez on 25/8/2023.
+//
+
+import SwiftUI
+
+struct StartUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartUpView()
+    }
+}
