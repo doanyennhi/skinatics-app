4a0f153a844b45295c08800cfd6fd894c7209821
doanyennhi <s3880599@rmit.edu.vn>
2023-08-27 06:08:18 +1000

    Delete unnecessary files

---
+++ Skinatics/Model/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  Skinatics
+//
+//  Created by Nhii on 27/8/2023.
+//
+
+import Foundation
+//
+//struct Product: Identifiable {
+//    var id = UUID().uuidString
+//    var image: String
+//    var brand: String
+//    var product: String
+//}
