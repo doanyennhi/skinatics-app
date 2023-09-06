df97cc1a9472ec988ca98e186e17a5a3a3cb0eb9
doanyennhi <s3880599@rmit.edu.vn>
2023-09-06 17:20:04 +1000

    Style ingredient dictionary screen

--- Skinatics/View/IngredientDictionary/IngredientCard.swift
+++ Skinatics/View/IngredientDictionary/IngredientCard.swift
@@ -15,15 +15,24 @@ struct IngredientCard: View {
     }
     
     var body: some View {
-        VStack {
-            Text(ingredient.name).font(.headline)
+        VStack(alignment: .leading) {
+            Text(ingredient.name)
+                .padding(.bottom, 5)
+                .font(Font.custom("Avenir", size: 24))
+                .bold()
+                .foregroundColor(Color("Dark Green"))
             Text(ingredient.description)
         }
+        .padding(20)
+        .background(.white)
+        .cornerRadius(15)
+        .shadow(color: .gray.opacity(0.2), radius: 10, x: 2, y: 2)
+        .padding(.bottom, 10)
     }
 }
 
 struct IngredientCard_Previews: PreviewProvider {
     static var previews: some View {
-        IngredientCard(ingredient: ingredients[0])
+        IngredientCard(ingredient: ingredients[1])
     }
 }
--- Skinatics/View/IngredientDictionary/IngredientDictView.swift
+++ Skinatics/View/IngredientDictionary/IngredientDictView.swift
@@ -10,9 +10,22 @@ import SwiftUI
 let ingredients = [Ingredient(name: "AHA", description: "AHA, also known as alpha hydroxy acid, exfoliates skin and improves signs of aging, dry skin and an uneven skin tone."), Ingredient(name: "Niacinamide", description: "Helps visibly minimize pore size, improve dull/uneven tone, and reduce wrinkle depth. Also known to fortify skin's barrier strength and boost hydration."), Ingredient(name: "Benzoyl Peroxide", description: "This over-the-counter topical antibacterial acne treatment is widely considered the most effective because of its ability to penetrate hair follicles and kill acne-causing bacteria with low sensitivity."), Ingredient(name: "Glycerin", description: "A naturally occurring substance in skin that helps retain moisture. It is used in skin care products to help defend against dryness.")]
 
 struct IngredientDictView: View {
+    private var ingredients: [Ingredient]
+    
+    init(ingredients: [Ingredient]) {
+        self.ingredients = ingredients
+    }
+    
     var body: some View {
         VStack {
             ScreenTitle(title: "Ingredient Dictionary")
+            ScrollView {
+                ForEach(ingredients) { ingredient in
+                    IngredientCard(ingredient: ingredient)
+                }
+            }
+            .padding(.vertical, 20)
+            .verticalFadeOut(fadeHeight: 50)
         }
         .modifier(ScreenModifier())
     }
@@ -20,6 +33,6 @@ struct IngredientDictView: View {
 
 struct IngredientDictView_Previews: PreviewProvider {
     static var previews: some View {
-        IngredientDictView()
+        IngredientDictView(ingredients: ingredients)
     }
 }
