6a69496c5724219fdab2a0210c8db3c3aa76dcfd
doanyennhi <s3880599@rmit.edu.vn>
2023-09-08 12:14:53 +1000

    Add styling and button for result screen

--- Skinatics/View/Camera/ResultsView.swift
+++ Skinatics/View/Camera/ResultsView.swift
@@ -9,30 +9,55 @@ import SwiftUI
 
 struct ResultsView: View {
     var body: some View {
-        ScrollView {
-            ScreenTitle(title: "Today's Results")
-            Text("What's this?")
-                .foregroundColor(Color("Grey"))
-                .font(Font.custom("Avenir", size: 16))
-                .underline()
-            
-            Text("Analysis")
-                .foregroundColor(Color("Dark Green"))
-                .font(Font.custom("Avenir", size: 22))
-                .bold()
-                .padding(.bottom, 10)
-            
-            VStack(alignment: .leading, spacing: 25) {
-                ResultSection(heading: "Hydration", content: "Your skin appears to be a little less hydrated. Look for products with hyaluronic acid to lock in moisture.")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                ScreenTitle(title: "Today's Results")
+                Text("What's this?")
+                    .foregroundColor(Color("Grey"))
+                    .font(Font.custom("Avenir", size: 16))
+                    .underline()
                 
-                ResultSection(heading: "Radiance", content: "We’re not seeing a change here yet. We recommend getting some good sleep every night and finding a product with vitamin C to help even out uneven skin tone.")
+                VStack {
+                    HStack {
+                        Text("Overall")
+                        Spacer()
+                        Text("74")
+                            .font(Font.custom("Avenir", size: 20))
+                    }
+                    .foregroundColor(Color("White"))
+                    .bold()
+                    .padding()
+                    .background(Color("Dark Green"))
+                    .clipShape(Capsule())
+                    .padding(.vertical, 20)
+                }
                 
-                ResultSection(heading: "Texture", content: "Your skin texture is getting better, however we still have to work on some texture like acne scars.")
+                Text("Analysis")
+                    .foregroundColor(Color("Dark Green"))
+                    .font(Font.custom("Avenir", size: 22))
+                    .bold()
+                    .padding(.vertical, 10)
                 
-                ResultSection(heading: "Lines", content: "We can see that there are no visible lines on your skin, great job preventing lines.")
+                VStack(alignment: .leading, spacing: 25) {
+                    ResultSection(heading: "Hydration", content: "Your skin appears to be a little less hydrated. Look for products with hyaluronic acid to lock in moisture.")
+                    
+                    ResultSection(heading: "Radiance", content: "We’re not seeing a change here yet. We recommend getting some good sleep every night and finding a product with vitamin C to help even out uneven skin tone.")
+                    
+                    ResultSection(heading: "Texture", content: "Your skin texture is getting better, however we still have to work on some texture like acne scars.")
+                    
+                    ResultSection(heading: "Lines", content: "We can see that there are no visible lines on your skin, great job preventing lines.")
+                }
+                .padding(.bottom, 30)
+                
+                NavigationLink(destination: CameraView()) {
+                    Text("Exit")
+                }
+                .buttonStyle(PrimaryButtonStyle())
+                .padding(.top, 30)
             }
+            .padding(.top, 1)
+            .modifier(ScreenModifier())
         }
-        .modifier(ScreenModifier())
     }
 }
 
