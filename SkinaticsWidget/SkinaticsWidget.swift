a0bd4ba72e567a887f6fe424fa3c94234d53b0fa
doanyennhi <s3880599@rmit.edu.vn>
2023-10-05 18:53:51 +1100

    Style widget based on size

--- SkinaticsWidget/SkinaticsWidget.swift
+++ SkinaticsWidget/SkinaticsWidget.swift
@@ -52,15 +52,126 @@ struct SkinaticsEntry: TimelineEntry {
     let productRating: Double
 }
 
-struct SkinaticsWidgetEntryView : View {
+struct SkinaticsWidgetSmallView: View {
     var entry: SkinaticsEntry
 
     var body: some View {
         ZStack {
             Color("WidgetBackground")
-            Text(entry.productName)
+//            VStack() {
+//                    Image(entry.productImg)
+//                        .resizable()
+//                        .aspectRatio(CGSize(width: 5, height: 4), contentMode: .fit)
+//                        .clipShape(RoundedRectangle(cornerRadius: 15))
+//                    Text(entry.productName)
+//                        .padding(10)
+//                        .background(Color("Green").opacity(0.8))
+//                        .foregroundColor(.white)
+//                        .bold()
+//                        .cornerRadius(15)
+//                        .lineLimit(1)
+//                        .padding([.bottom, .horizontal], 5)
+//                }
+//                .padding(10)
+            
+            ZStack(alignment: .bottom) {
+                    Image(entry.productImg)
+                        .resizable()
+                        .scaledToFit()
+                    Text(entry.productName)
+                        .padding(10)
+                       .background(Color("Green").opacity(0.8))
+                        .foregroundColor(.white)
+                        .bold()
+                        .cornerRadius(15)
+                        .lineLimit(1)
+                        .padding([.bottom, .horizontal], 5)
+                }
         }
-    }
+        }
+}
+
+struct SkinaticsWidgetMediumView: View {
+    var entry: SkinaticsEntry
+
+    var body: some View {
+        ZStack {
+            Color("WidgetBackground")
+            
+            VStack() {
+                Text("Today's Product")
+                HStack() {
+                    Image(entry.productImg)
+                        .resizable()
+                        .aspectRatio(CGSize(width: 5, height: 4), contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(entry.productName)
+                            .bold()
+                            .foregroundColor(Color("Green"))
+                        HStack {
+                            Text("\(entry.productRating.formatted())")
+                            Image(systemName: "star.fill")
+                        }
+                    }
+                    .padding(.leading, 10)
+                }
+            }
+            .padding(.horizontal, 2)
+            .padding(.vertical, 10)
+        }
+        }
+}
+
+struct SkinaticsWidgetLargeView: View {
+    var entry: SkinaticsEntry
+
+    var body: some View {
+        ZStack {
+            Color("WidgetBackground")
+            VStack() {
+                ZStack(alignment: .bottom) {
+                    Image(entry.productImg)
+                        .resizable()
+
+                    Text(entry.productName)
+                        .padding(10)
+                        .background(Color("Green").opacity(0.8))
+                        .foregroundColor(.white)
+                        .bold()
+                        .cornerRadius(10)
+                        .lineLimit(1)
+
+                    HStack {
+                        Text("\(entry.productRating.formatted())")
+                        Image(systemName: "star.fill")
+                    }
+                }
+            }
+            .padding(.top, 5)
+        }
+        }
+}
+
+
+struct SkinaticsWidgetEntryView : View {
+    var entry: SkinaticsEntry
+
+    @Environment(\.widgetFamily) var widgetFamily: WidgetFamily
+    @ViewBuilder
+    var body: some View {
+        switch widgetFamily {
+                case .systemSmall:
+                    SkinaticsWidgetSmallView(entry: entry)
+                case .systemMedium:
+                    SkinaticsWidgetMediumView(entry: entry)
+                case .systemLarge:
+                    SkinaticsWidgetLargeView(entry: entry)
+                default:
+                    EmptyView()
+                }
+        }
 }
 
 struct SkinaticsWidget: Widget {
@@ -72,12 +183,13 @@ struct SkinaticsWidget: Widget {
         }
         .configurationDisplayName("Skinatics Widget")
         .description("This is a widget to display one skincare product of the day.")
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
     }
 }
 
 struct SkinaticsWidget_Previews: PreviewProvider {
     static var previews: some View {
-        SkinaticsWidgetEntryView(entry: SkinaticsEntry(date: Date(), productName: "Product", productImg: "", productRating: 4.0))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        SkinaticsWidgetEntryView(entry: SkinaticsEntry(date: Date(), productName: "Hyaluronic Acid 2% + B5", productImg: "the-ordinary-hyaluronic-acid-style", productRating: 4.2))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
